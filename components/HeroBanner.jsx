408b8b8e7b56795fbc0a0025f2cdd81d750ef61c
faisal <faisalfirdani01@gmail.com>
2022-12-10 22:30:59 +0700

    Finishing Product component

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import { urlFor } from '../lib/client'
 
 const HeroBanner = ({heroBanner}) => {
-    console.log(heroBanner)
   return (
     <div className='hero-banner-container'>
         <div>
